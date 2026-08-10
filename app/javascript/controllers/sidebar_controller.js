9f232887fb07cc4793b2b901f3a348ecab786b4c
demetrius albuquerque <demetrius.albuquerque@yahoo.com.br>
2026-08-11 01:19:57 +0200

    fix: persist sidebar collapsed state across navigations

---
+++ app/javascript/controllers/sidebar_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static STORAGE_KEY = "lakedeepdiver:sidebar-collapsed"
+
+  static targets = ["collapse"]
+
+  connect() {
+    if (localStorage.getItem(this.constructor.STORAGE_KEY) === "true") {
+      this.element.classList.add("collapsed")
+    }
+  }
+
+  toggle(event) {
+    this.element.classList.toggle("collapsed")
+    localStorage.setItem(
+      this.constructor.STORAGE_KEY,
+      this.element.classList.contains("collapsed") ? "true" : "false"
+    )
+  }
+}
